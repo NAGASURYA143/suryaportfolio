867745825b83eeb65e48654512b7b21b2a0a07e5
surya <nagasuryachennuri@gmail.com>
2025-08-09 22:29:37 +0530

    v4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfoliosurya/', // This should match your GitHub repo name
+  base: '/suryaportfolio/', // This should match your GitHub repo name
 });
